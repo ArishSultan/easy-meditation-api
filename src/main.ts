1706b69e4ef2fb86df8e2ce881050b26fd989e00
arish <arishkhan28.08@gmail.com>
2021-05-11 10:04:32 +0500

    added https support

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,18 @@
+import { readFileSync, existsSync } from 'fs';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const httpsOptions = {};
+  if (existsSync('/etc/ssl/certs/nginx-selfsigned.crt')) {
+    httpsOptions['key'] = readFileSync('/etc/ssl/private/nginx-selfsigned.key');
+    httpsOptions['cert'] = readFileSync('/etc/ssl/certs/nginx-selfsigned.crt');
+  }
+
+  const app = await NestFactory.create(AppModule, { httpsOptions });
   app.enableCors();
+
   await app.listen(process.env.PORT || 4000);
 }
+
 bootstrap().then(() => ({}));
