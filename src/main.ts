f7568cee96a2b64d3b5f3da867d77c1250073441
arish <arishkhan28.08@gmail.com>
2021-05-15 01:02:14 +0500

    removed ssl

--- src/main.ts
+++ src/main.ts
@@ -1,24 +1,8 @@
-import { existsSync, readFileSync } from 'fs';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-  const httpsOptions = {};
-
-  if (
-    existsSync(
-      '/etc/letsencrypt/live/dashboard.easymeditation.co/fullchain.pem',
-    )
-  ) {
-    httpsOptions['key'] = readFileSync(
-      '/etc/letsencrypt/live/dashboard.easymeditation.co/privkey.pem',
-    );
-    httpsOptions['cert'] = readFileSync(
-      '/etc/letsencrypt/live/dashboard.easymeditation.co/fullchain.pem',
-    );
-  }
-
-  const app = await NestFactory.create(AppModule, { httpsOptions });
+  const app = await NestFactory.create(AppModule);
   app.enableCors();
 
   await app.listen(process.env.PORT || 4000);
